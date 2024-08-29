d52d853c11c92e9dfb3dcd21474593d6373af75a
Pablo Peralta <pperalta@asap-consulting.net>
2024-08-29 11:45:17 -0300

    that is not html comment

--- src/app/components/countries-list/countries-list.component.ts
+++ src/app/components/countries-list/countries-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'home-countries-list',
@@ -9,4 +9,7 @@ import { Component } from '@angular/core';
 })
 export class CountriesListComponent {
 
+  @Input() countries: any[] = []; 
+
+  constructor() {}
 }
--- src/app/components/searcher/searcher.component.ts
+++ src/app/components/searcher/searcher.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { ApiCountryService } from '../../services/api-country.service';
+import { debounceTime, switchMap } from 'rxjs';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'home-searcher',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './searcher.component.html',
   styleUrl: './searcher.component.css'
 })
-export class SearcherComponent {
+export class SearcherComponent implements OnInit {
+  @Output() searchResult = new EventEmitter<any>(); 
 
+  countryForm: FormGroup = new FormGroup({
+    name: new FormControl(''),
+  });
+
+  constructor(private apiCountryService: ApiCountryService) {} 
+
+  ngOnInit(): void {
+ 
+    this.countryForm.get('name')?.valueChanges
+      .pipe(
+        debounceTime(300), 
+        switchMap(value => this.apiCountryService.getCountryByName(value))
+      )
+      .subscribe(result => {
+        this.searchResult.emit(result);
+      });
+  }
+
+  onSearch(): void {
+    const countryName = this.countryForm.get('name')?.value;
+    this.apiCountryService.getCountryByName(countryName).subscribe(result => {
+      this.searchResult.emit(result); 
+    });
+  }
 }
