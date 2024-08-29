6d789853b0988f18e131b506f05a03e03d97d7e3
Pablo Peralta <pperalta@asap-consulting.net>
2024-08-29 09:32:32 -0300

    adding routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: inicio ,
+        children: [
+            {
+                path: '/capitales',
+                title: 'capitales',
+                component: "componente",
+            },
+            {
+                path: '/paises',
+                title: 'paises',
+                component: "componentepais"
+            },
+            {
+                path: '/ciudad',
+                title: 'ciudades',
+                component: "componente"
+            }
+        ]
+    },
+    {path: 'home', component: home},
+];
